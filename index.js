c7c4857bda4bd5f694fb6b3119e2895bc5343bfa
Alucard <spdauditoria@gmail.com>
2020-10-11 15:50:18 -0300

    Fix Layout

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import DeviceInfo from "react-native-device-info";
 import * as RNLocalize from "react-native-localize";
 import { formatDate, subscriptionRequestAdapter, showAlertLink } from "./utils";
 import ListenToNotifications, { linkInApp } from "./ListenToNotifications";
-import { subscription,notificationApi } from "./inngageApi";
+import { subscription, notificationApi } from "./inngageApi";
 import AsyncStorage from '@react-native-community/async-storage';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 import React, { useState, useEffect, useRef } from 'react';
@@ -37,41 +37,89 @@ const ITEM_HEIGHT = Math.round(SLIDER_HEIGHT * 0.8);
 export const Inapp = (props) => {
   const [data, setData] = useState([])
   const [ind, setInd] = useState(0)
+  const [indIm, setIndImg] = useState(0)
   const [visible, setVisible] = useState(true)
+  const [modalImage, setModalImage] = useState(false)
   const [bgImage, setbgImage] = useState(undefined)
 
-  pagination = () => {
-    return (
-      <Pagination
-        dotsLength={data.length}
-        activeDotIndex={ind}
-        dotStyle={{
-          width: 5,
-          height: 5,
-          borderRadius: 5,
-          marginHorizontal: 8,
-          backgroundColor: 'rgba(0, 0, 0, 0.92)'
-        }}
-        inactiveDotStyle={{
-          // Define styles for inactive dots here
-        }}
-        inactiveDotOpacity={0.4}
-        inactiveDotScale={0.6}
-      />
-    );
-  }
+  const CarouselRef = useRef(null);
+  const ScrollRef1 = useRef(null);
+  const ScrollRef2 = useRef(null);
+  const ScrollRef3 = useRef(null);
+
 
   _renderItem = ({ item, index }) => {
     let msg = JSON.parse(item.data.additional_data)
     let arrayImgs = []
+    let indImg = 0
+    console.log(index)
+    chooseRef = () => {
+      if (index == 0) {
+        return ScrollRef1
+      }
+      if (index == 1) {
+        return ScrollRef2
+      }
+      if (index == 2) {
+        return ScrollRef3
+      }
+    }
 
-    const handleButton = (title,body,url,type) => {
-      if (type === '' || url ==='') {
+
+    pagination = ref => {
+      return (
+        <Pagination
+          dotsLength={arrayImgs.length}
+          activeDotIndex={indIm}
+          containerStyle={{ height: 2, padding: 0, margin: 0 }}
+          renderDots={(activeIndex, total, context) => {
+            let dots = []
+            var size = 0
+            for (let i = 0; i < total; i++) {
+              if (activeIndex == i) {
+                size = 13
+              } else {
+                size = 8
+              }
+              dots.push(
+                <TouchableOpacity
+                  onPress={() => {
+                    ref.current.scrollTo({ x: i * 220, y: 0, animated: true })
+                    if (i * 220 === 0) {
+                      setIndImg(0)
+                    } else if (i * 220 === 220) {
+                      setIndImg(1)
+                    } else if (i * 220 === 440) {
+                      setIndImg(2)
+                    }
+                  }}
+                  key={i.toString()}
+                  style={{
+                    backgroundColor: 'white',
+                    borderRadius: 100,
+                    width: size,
+                    height: size,
+                    marginLeft: 5
+                  }}
+                />
+              )
+            }
+            return (
+              dots
+            )
+          }
+          }
+        />
+      );
+    }
+
+    const handleButton = (title, body, url, type) => {
+      if (type === '' || url === '') {
         return
       }
-      console.log(title,body,url,type)
+      console.log(title, body, url, type)
       const openLinkByType = (type, url) => (type === 'deep' ? Linking.openURL(url) : linkInApp(url))
-      
+
       return Linking.canOpenURL(url).then((supported) => {
         if (supported) {
           showAlertLink(
@@ -95,8 +143,8 @@ export const Inapp = (props) => {
         if (msg.rich_content.img3 != '') {
           arrayImgs.push({ url: msg.rich_content.img3 })
         }
-        let arrayElements = arrayImgs.map((item,index )=> (
-          <Image key={index.toString()} style={[props.mediaStyle, { width: 200, height: 200, marginBottom: 10 }]} source={{ uri: item.url }} />
+        let arrayElements = arrayImgs.map((item, index) => (
+          <Image key={index.toString()} style={[props.mediaStyle, { width: 200, height: 200, marginRight: 10 }]} source={{ uri: item.url }} />
         ));
         return arrayElements
       } else if (arrayImgs.length <= 0) {
@@ -121,18 +169,23 @@ export const Inapp = (props) => {
       btn_left: {
         backgroundColor: msg.btn_left_bg_color,
         height: 40,
-        flex: 1,
+        width: 100,
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 20,
+        marginRight: 20,
+        marginLeft: 10,
+        flex: 1
       },
       btn_right: {
         backgroundColor: msg.btn_right_bg_color,
         height: 40,
-        flex: 1,
+        width: 100,
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 20,
+        marginRight: 10,
+        flex: 1
       },
       btn_left_title: {
         color: msg.btn_left_txt_color
@@ -150,7 +203,7 @@ export const Inapp = (props) => {
       bodyText: {
         color: msg.body_font_color,
         textAlign: 'justify',
-        marginTop: 10,
+        marginBottom: 10,
         fontSize: 15,
       },
       title: {
@@ -163,32 +216,53 @@ export const Inapp = (props) => {
     return (
       <View style={[itemStyles.body]}>
         <Text style={[itemStyles.title, props.titleStyle]}>{msg.title}</Text>
-        <ScrollView style={{ height: 200 }}>
+        <ScrollView
+          ref={chooseRef()}
+          horizontal
+          snapToInterval={220}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          style={{ width: 200 }}
+          onMomentumScrollEnd={(e) => {
+            if (Math.round(e.nativeEvent.contentOffset.x) === 0
+            ) {
+              indImg = 0
+              setIndImg(indImg)
+            }
+            if (Math.round(e.nativeEvent.contentOffset.x) === 220
+            ) {
+              indImg = 1
+              setIndImg(indImg)
+            }
+            if (Math.round(e.nativeEvent.contentOffset.x) === 430
+            ) {
+              indImg = 2
+              setIndImg(indImg)
+            }
+          }}
+        >
           {imgCarosel()}
         </ScrollView>
+        {pagination(chooseRef())}
         <Text style={[itemStyles.bodyText, props.bodyStyle]}>{msg.body}</Text>
-        <Text style={[styles.counter, props.counterStyle]}>
+        {/* <Text style={[styles.counter, props.counterStyle]}>
           {ind + 1} de {data.length} Mensagens
-            </Text>
-        {/* {pagination()} */}
-        <View style={{ flexDirection: "row", marginBottom: 10 }}>
-          <TouchableOpacity onPress={()=> handleButton(msg.title, msg.body,msg.btn_left_action_link,msg.btn_left_action_type)} style={[itemStyles.btn_left, props.buttonTitleRightStyle]}>
+        </Text> */}
+        <View style={{ flexDirection: "row", marginBottom: 0, justifyContent: 'center' }}>
+          <TouchableOpacity onPress={() => handleButton(msg.title, msg.body, msg.btn_left_action_link, msg.btn_left_action_type)} style={[itemStyles.btn_left, props.buttonTitleRightStyle]}>
             <View>
               <Text style={[itemStyles.btn_left_title, props.buttonTitleLeftStyle]}>{msg.btn_left_txt}</Text>
             </View>
           </TouchableOpacity>
-          <TouchableOpacity onPress={()=> handleButton(msg.title, msg.body,msg.btn_right_action_link,msg.btn_right_action_type)} style={[itemStyles.btn_right, props.button_right]}>
+          <TouchableOpacity onPress={() => ScrollRef1.current.scrollTo({ x: 220, y: 0, animated: true })} style={[itemStyles.btn_right, props.button_right]}>
             <View>
               <Text style={[itemStyles.btn_right_title, props.buttonTitleRightStyle]}>{msg.btn_right_txt}</Text>
             </View>
           </TouchableOpacity>
         </View>
-
       </View>
     );
   }
-
-  const CarouselRef = useRef();
 
   useEffect(() => {
     onLoad();
@@ -233,43 +307,46 @@ export const Inapp = (props) => {
 
   if (data.length > 0) {
     return (
-      <Modal
-        animationType='slide'
-        visible={visible}
-        transparent={true}
-        style={{ flex: 1 }}
-      >
-        <View style={[styles.styleContainer, props.styleContainer]}>
-          <ImageBackground style={{ widht: '100%', alignItems: 'center' }} imageStyle={{borderRadius:10}} source={bgImage}>
-            <TouchableHighlight
-              onPress={() => handleClose()}
-              underlayColor='#cccccc'
-              style={styles.closeButton}
-            >
-              <Text style={{ fontWeight: 'bold' }}>
-                X
-            </Text>
-            </TouchableHighlight>
-            <Carousel
-              style={{ alignSelf: 'stretch' }}
-              ref={CarouselRef}
-              layout={'default'}
-              layoutCardOffset={10}
-              data={data}
-              inactiveSlideOpacity={0}
-              containerCustomStyle={styles.carouselContainer}
-              inactiveSlideShift={0}
-              onSnapToItem={(index) => {
-                setInd(index);
-                handleBg(index)
-              }}
-              renderItem={_renderItem}
-              sliderWidth={SLIDER_WIDTH}
-              itemWidth={ITEM_WIDTH}
-            />
-          </ImageBackground>
-        </View>
-      </Modal>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Modal
+          animationType='slide'
+          visible={visible}
+          transparent={true}
+          style={{ justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue' }}
+        ><View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={[styles.styleContainer, props.styleContainer]}>
+              <ImageBackground style={{ widht: '100%' }} imageStyle={{ borderRadius: 10, alignSelf: 'center' }} source={bgImage}>
+                <TouchableHighlight
+                  onPress={() => handleClose()}
+                  underlayColor='#cccccc'
+                  style={styles.closeButton}
+                >
+                  <Text style={{ fontWeight: 'bold' }}>
+                    X
+                  </Text>
+                </TouchableHighlight>
+                <Carousel
+                  vertical
+                  ref={CarouselRef}
+                  layout={'default'}
+                  layoutCardOffset={10}
+                  data={data}
+                  inactiveSlideOpacity={0}
+                  containerCustomStyle={styles.carouselContainer}
+                  inactiveSlideShift={0}
+                  onSnapToItem={(index) => {
+                    setInd(index);
+                    handleBg(index)
+                  }}
+                  renderItem={(item, index) => _renderItem(item, index, CarouselRef)}
+                  sliderHeight={SLIDER_WIDTH}
+                  itemHeight={ITEM_WIDTH}
+                />
+              </ImageBackground>
+            </View>
+          </View>
+        </Modal>
+      </View>
     );
   } else {
     return null
@@ -285,23 +362,24 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     elevation: 10,
     borderRadius: 10,
-    margin: 20
+    width: SLIDER_WIDTH * 0.8,
   },
   counter: {
     alignSelf: 'center',
     marginVertical: 10
   },
   closeButton: {
+    position: 'absolute',
     elevation: 10,
     alignSelf: 'flex-end',
-    margin: 10,
+    right: 20,
+    top: 20,
     width: 30,
     height: 30,
     borderRadius: 20,
     backgroundColor: '#f2f2f2',
     justifyContent: 'center',
     alignItems: 'center',
-    zIndex: 9999
   },
 });
 
